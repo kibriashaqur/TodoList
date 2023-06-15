a33d7e3140a05d09a67136b51d2a3ac4535d8eaa
Golam Kibria Shaqur <2018-1-60-237@std.ewubd.edu>
2023-06-15 11:22:07 +0600

    [MOD] home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,7 +11,23 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text(
+          "To Do List",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          Padding(padding: EdgeInsets.only(right: 10),
+          child: Icon(
+          Icons.help_outline_rounded,
+          ),
+          )],
+      ),
     );
   }
 }
