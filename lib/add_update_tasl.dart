94b0baff27a66022632cdcaa272e4f435e433718
Golam Kibria Shaqur <2018-1-60-237@std.ewubd.edu>
2023-06-15 11:47:33 +0600

    [MOD] home_screen.dart and [ADD] add_update_task.dart

---
+++ lib/add_update_tasl.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddUpdateTask extends StatefulWidget {
+  const AddUpdateTask({super.key});
+
+  @override
+  State<AddUpdateTask> createState() => _AddUpdateTaskState();
+}
+
+class _AddUpdateTaskState extends State<AddUpdateTask> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'add_update_tasl.dart';
+import 'dbHelper.dart';
+import 'list.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DBHelper? dbHelper;
+  late Future<List<TodoList>> dataList;
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper();
+    loadData();
+  }
+
+  loadData() {
+    dataList = dbHelper!.getDataList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,16 +32,50 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.bold,
+            letterSpacing: 1,
           ),
         ),
         centerTitle: true,
         elevation: 0,
         actions: [
-          Padding(padding: EdgeInsets.only(right: 10),
-          child: Icon(
-          Icons.help_outline_rounded,
-          ),
-          )],
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.help_outline_rounded,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: FutureBuilder(
+            future: dataList,
+            builder: (context, AsyncSnapshot<List<TodoList>> snapshot) {
+              if (!snapshot.hasData || snapshot.data == null) {
+                return Center(
+                  child: Text(
+                    "No Task Fount",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                );
+              }else {
+                return Container();
+              }
+            },
+          )),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton (
+        backgroundColor: Colors.blue,
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(
+              builder: (context) => AddUpdateTask()));
+        },
       ),
     );
   }
